2f72ee319c96f230f542a2857cef0bfda8095933
Max Savin <its@maxsavin.com>
2017-06-28 11:01:01 +0200

    Added history tracking; started automatically tracking userIds when possible; added resolveAll() for items that have the same data in them

--- queue.js
+++ queue.js
@@ -1,8 +1,8 @@
 Queue = {}
 
 Queue.config = {}
-
-Queue.collection = new Mongo.Collection("queue");
+Queue.pending = new Mongo.Collection("queuePending");
+Queue.posted = new Mongo.Collection("queuePosted");
 
 Queue.register = function (config) {
 	keys = Object.keys(config);
@@ -14,14 +14,35 @@ Queue.register = function (config) {
 
 Queue.add = function (name, data) {
 	if (Queue.config[name])	{
-		Queue.collection.insert({
+		doc = {
 			name: name,
 			data: data,
 			date: new Date()
-		});
+		}
+
+		if (Meteor.userId()) {
+			doc.userId = Meteor.userId();
+		}
+
+		Queue.collection.insert(doc);
 	} else {
 		console.log("Queue Error: '" + name + "' does not exist");
 	}
+}
+
+Queue.addToHistory = function (doc, action, extraData, result) {
+	doc = {
+		original: doc,
+		action: action,
+		extraData: extraData,
+		result: result
+	};
+
+	if (Meteor.userId) {
+		userId: Meteor.userId();
+	}
+
+	Queue.posted.insert(doc)
 }
 
 Queue.resolve = function (id, action, extraData) {
@@ -35,7 +56,12 @@ Queue.resolve = function (id, action, extraData) {
 		// Assume the remove returns 1, we can assume we are the only ones to touch it
 		if (remove === 1) {
 			try  {
-				return Queue.config[doc.name][action](doc.data, extraData);
+				result = Queue.config[doc.name][action](doc.data, extraData);
+				// assuming the function above ran successfully, we add the item to history
+				if (result) {
+					Queue.addToHistory(doc, action, extraData, result)
+				}
+				return result;
 			} catch (e) {
 				// If the function failed for whatever reason, we put the item back in the queue
 				Queue.collection.insert(doc);
@@ -45,4 +71,13 @@ Queue.resolve = function (id, action, extraData) {
 		console.log("Queue Error: item '" + id + "' was not found.");
 		return false;
 	}
+}
+
+Queue.resolveAll = function (id, action, extraData) {
+	doc = Queue.collection.findOne(id)
+	doc.data;
+
+	docs.find({data: docData}).fetch().forEach(function (item) {
+		Queue.resolve(item._id, action, extraData)
+	});
 }
